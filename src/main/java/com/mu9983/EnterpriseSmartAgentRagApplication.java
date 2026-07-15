f5741f02c3a811fbff2ad8aaea7ff1be88145484
Mu9983 <15246452885@163.com>
2026-07-15 10:09:44 +0800

    初始创建项目

---
+++ src/main/java/com/mu9983/EnterpriseSmartAgentRagApplication.java
@@ -0,0 +1,13 @@
+package com.mu9983;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseSmartAgentRagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseSmartAgentRagApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mu9983/EnterpriseSmartAgentRagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mu9983;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseSmartAgentRagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
