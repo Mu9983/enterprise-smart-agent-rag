5ce2919e1ea922e2c8b04ab14c1130c901cf0e17
Mu9983 <15246452885@163.com>
2026-07-15 16:13:29 +0800

    拦截器

---
+++ src/main/java/com/mu9983/filter/TokenFilter.java
@@ -0,0 +1,40 @@
+package com.mu9983.filter;
+
+import com.mu9983.utils.JwtUtils;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+@Slf4j
+public class TokenFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        String token = request.getHeader("token");
+        String requestURI = request.getRequestURI();
+        if (requestURI.contains("/login")) {
+            log.info("登录请求，放行");
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+        if (token == null || token.isEmpty()) {
+            log.error("令牌为空，拦截");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+        try {
+            JwtUtils.parseToken(token);
+        } catch (Exception e) {
+            log.error("令牌有误，拦截");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+        log.info("令牌合法，放行");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
